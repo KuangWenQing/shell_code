463e2896645ce8a7da3d4390f319d6589ca97b78
jqiu <you@example.com>
2020-11-25 11:07:43 +0800

    1、添加gga转kml 2、添加自动执行位置分析py 3、添加google地图标点

--- get_nmea.sh
+++ get_nmea.sh
@@ -5,37 +5,86 @@ then
 	echo "please input the dir"
 	exit 1
 fi
-echo 'Now deal with the directory -- '"'$1'"
+echo '--------- Now deal with the directory -- '"'$1'"
 
 cd $1
-mkdir nmea
-if [ $? -ne 0 ]
-then
-	echo "the file named 'nmea' is exit"
-	exit 1
+if [ ! -d "./chart" ]; then
+	mkdir chart
 fi
 
-for file in `ls *.log *.ubx`
-do
-	if [ ${file##*.} == "ubx" ]
-	then
-		grep "E,1," $file -a > nmea/${file%.*}".tmp"
-		`awk 'BEGIN{FS="GGA,"} {print $2}' nmea/${file%.*}".tmp" > nmea/${file%.*}".nmea"`
-		rm nmea/${file%.*}".tmp"
-		sed -i "s/^/\$GPGGA,/" nmea/${file%.*}".nmea"
-#		sed -i "s/^\(.*\)\$GN/\$GP/" nmea/${file%.*}".nmea"
-	else
-		grep "E,1," $file | grep "KF" > nmea/${file%.*}"_KF.nmea"
-		grep "E,1," $file | grep ",\*" > nmea/${file%.*}"_noKF.nmea"
-		sed -i "s/\r//g;s/ok//g" nmea/${file%.*}"_noKF.nmea"
-		sed -i "s/ok//g" nmea/${file%.*}"_KF.nmea"
+for file in `ls *_F9P.ubx`
+do 
+	F9P_file=${file%.*}".txt"
+	if [ ! -f "$F9P_file" ]; then
+		/home/jqiu/share/a.out $file > ${file%.*}".txt"
 	fi
 done
+echo -e "--------- transformation ubx file successful ---------------\n"
 
+path=`pwd`
+
+if [ ! -d "./nmea" ]; then
+	mkdir nmea
+	for file in `ls *.log *.ubx *.DAT`
+	do
+		if [ ${file##*.} == "ubx" ]
+		then
+			grep "E,1," $file -a > nmea/${file%.*}".tmp"
+			`awk 'BEGIN{FS="GGA,"} {print $2}' nmea/${file%.*}".tmp" > nmea/${file%.*}".nmea"`
+			rm nmea/${file%.*}".tmp"
+			sed -i "s/^/\$GPGGA,/" nmea/${file%.*}".nmea"
+		#	sed -i "s/^\(.*\)\$GN/\$GP/" nmea/${file%.*}".nmea"
+		elif [ ${file##*.} == "DAT" ]
+		then
+			grep "E,1," $file | grep "KFP" > nmea/${file%.*}"_KF.nmea"
+                        grep "E,1," $file | grep "GGA" > nmea/${file%.*}"_noKF.nmea"
+                        sed -i "s/\r//g;s/ok//g" nmea/${file%.*}"_noKF.nmea"
+                        sed -i "s/ok//g" nmea/${file%.*}"_KF.nmea"
+		else
+			grep "E,1," $file | grep "KF" > nmea/${file%.*}"_KF.nmea"
+			grep "E,1," $file | grep ",\*" > nmea/${file%.*}"_noKF.nmea"
+			sed -i "s/\r//g;s/ok//g" nmea/${file%.*}"_noKF.nmea"
+			sed -i "s/ok//g" nmea/${file%.*}"_KF.nmea"
+		fi
+	done
+fi
+echo -e "--------- get nmea(gga) files successfully -------------\n"
+/home/jqiu/PycharmProjects/data_handle/AVE_ALL_to_GGA.py
+mv *.gga ./nmea
+
+/home/jqiu/PycharmProjects/data_handle/header_file.py $path'/' $F9P_file
+
+rm kml -rf
 mkdir kml
 for file in `ls nmea/`
 do
-	~/nmea2kml.py "nmea/"$file > kml/${file%.*}".kml"
+	/home/jqiu/nmea2kml.py "nmea/"$file > kml/${file%.*}".kml"
+done
+cd kml
+for file in `ls *M8T.kml`
+do
+	sed -i '$d' $file	# delete "</Document>"
+	sed -i '$d' $file	# delete "</kml>"
+done
+cd ..
+echo -e "--------- get kml files successfully --------------\n"
+
+cd kml
+M8T_file=$(ls -l *_M8T.kml | awk '{print $9}')
+cd ..
+
+
+for file in `ls *.log`
+do
+	echo $file
+	final_xyz=`grep "DEBUG R AVE, tot" $file | tail -n 1`
+	if [ "$M8T_file" =  "" ]; then
+		/home/jqiu/PycharmProjects/data_handle/analysis_final_pos.py $path'/' $F9P_file "$final_xyz"
+	else
+		/home/jqiu/PycharmProjects/data_handle/add_pos_to_kml.py $path'/' $F9P_file "$final_xyz" $M8T_file $file
+	fi
 done
 
+echo '</Document>' >> 'kml/'$M8T_file
+echo '</kml>' >> 'kml/'$M8T_file
 
